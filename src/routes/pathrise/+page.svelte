68510dbd77bda7b171cdde56a23c76f50d19631e
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2024-05-06 16:54:04 -0700

    Responsive Design

--- src/routes/pathrise/+page.svelte
+++ src/routes/pathrise/+page.svelte
@@ -12,17 +12,19 @@
 <div class="w-[80%] flex flex-wrap m-auto">
     <div class="videoContainer">
         <h1 class="underline w-full">Pathrise Submissions</h1>
-        <h2>Track-Specific Video</h2>
-        <caption>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/GO2jojJdmuE?si=EkMun6abS-Sarwfs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            All Three LeetCode questions were answered within this video.
-            <br>Video is longer than expected due to understanding and processing each question.
-        </caption>
+        <div class="m-auto">
+            <h2>Track-Specific Video</h2>
+            <caption class="m-auto">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/GO2jojJdmuE?si=EkMun6abS-Sarwfs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                All Three LeetCode questions were answered within this video.
+                <br>Video is longer than expected due to understanding and processing each question.
+            </caption>
+        </div>
     </div>
 
     <div class="videoContainer">
         <h2>General Video</h2>
-        <caption>
+        <caption class="m-auto">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/QY2b2KgpiNQ?si=ixSa8_pHU_1Zdu75" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             Elaboration on my background, experiences, positions, and current projects. 
         </caption>
